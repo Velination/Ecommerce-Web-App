5f2d1f939fe70e92bcc87ea85fa8fa77a7a45f70
Velination <143023447+Velination@users.noreply.github.com>
2024-04-06 00:54:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+// The products on slides on the second border
 const products = [
   {
     id: 1,
     title: "Air Force",
-    price: 119,
+    price: $119,
     colors: [
       { code: "black", img: "img/air.png" },
       {
@@ -15,11 +16,77 @@ const products = [
       },
     ],
   },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: $149,
+    colors: [
+      { code: "lightgray", img: "img/jordan.png" },
+      {
+        code: "green",
+        img: "img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: $109,
+    colors: [
+      { code: "lightgray", img: "img/blazer.png" },
+      {
+        code: "green",
+        img: "img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: $129,
+    colors: [
+      { code: "black", img: "img/crater.png" },
+      {
+        code: "lightgray",
+        img: "img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: $99,
+    colors: [
+      { code: "gray", img: "img/hippie.png" },
+      {
+        code: "black",
+        img: "img/hippie2.png",
+      },
+    ],
+  },
 ];
+
+// change the details on the slides
+let chosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorALL(".size");
 
 // for making the front page slide through
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    // change the current slide
     wrapper.style.transform = `translatex(${-100 * index}vw)`;
+
+    // // change the chosen product
+    // chosenProduct = products[index];
+
+    // // change texts of currentProduct
+    // currentProductTitle.textContent = chosenProduct.title;
+    // currentProductPrice.textContent = chosenProduct.price;
+    // currentProductImg.src = chosenProduct.colors[0].img;
   });
 });
